f54ece477b753601c9d9e86c1c43d6eb40f840e3
Dillon Jason <djasuar@gmail.com>
2021-04-18 09:35:10 -0400

    Update to used shared eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "prettier"],
+  extends: ["dillonjason", "dillonjason/typescript"],
 };
